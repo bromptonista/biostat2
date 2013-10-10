c483d9ffdf43019958088a517ccbb30f69e30b4d
Noah Peart <noah.v.peart@gmail.com>
2013-10-10 16:44:00 -0400

    wk2

---
+++ eqs.R
@@ -0,0 +1,12 @@
+## Standard Error: SE = SD / sqrt(n), n = sample size
+
+## Confidence intevals to standard deviations
+## For, two-sided 95%: (get width of conf. interval) => qnorm(0.025, lower.tail=FALSE) * 2
+##  Then: SD = sqrt(n) * (upper limit - lower limit) / 3.92
+##  n = sample size, 3.92 is width of conf. interval from standard normal
+
+## Two sample tests - two independent groups
+## (X - Y) / (S_p * sqrt(1/n_x + 1/n_y)), S_p pooled sd
+## (S_p)^2 = ((n_1-1)^2 * S_1^2 + (n_2 - 1)^2 * S_2^2 + ... ) / ((n_1 - 1) + (n_2 - ) + ... )
+
+
--- qz2.R
+++ qz2.R
@@ -1,3 +1,5 @@
+## Question 1:
+## 11.3 or lower
 
 ## Question 2:
 ## A pharmaceutical company is interested in testing a potential blood pressure lowering medication. Their first examination considers only subjects that received the medication at baseline then two weeks later. The data are as follows (SBP in mmHg)
@@ -38,3 +40,66 @@ cor1 <- cor(bpress[,1], bpress[,2]) ## correlation = 1
 
 ## Question 3:
 ## Brain volumes for 9 men yielded a 90 % confidence interval of 1,077 cc to 1,123 cc. Would you reject in a two sided 5% hypothesis test of H0:¦Ì=1,078?
+## testStat <= abs(2 * qt(.1, 8)
+mean1 <- (1077 + 1123) /  2 ## 1100
+zwid <- qnorm(0.025) * 2 ## standard normal conf width
+twid <- qt(0.025, 8, lower.tail = FALSE) * 2 ## t conf width
+tsd <- sqrt(9) * (1123 - 1077) / twid
+testStat <- abs(sqrt(9) * (1078 - 1100) / tsd)
+testStat <= abs(qt(0.025, 8) * 2)
+## Fail to reject
+
+## Question 4:
+## In an effort to improve efficiency, hospital administrators are evaluating a new triage system for their emergency room. In an validation study of the system, 5 patients were tracked in a mock (simulated) ER under the new and old triage system. Their waiting times in natural log of hours were:
+triage <- read.table(text = "Subject 	1 	2 	3 	4 	5
+New 	0.929 	-1.745 	1.677 	0.701 	0.128
+Old 	2.233 	-2.513 	1.204 	1.938 	2.533", header = TRUE, sep = "\t")
+## Give a Pvalue for the test of the hypothesis that the new system resulted in lower waiting times for a one sided t test.
+dif <- as.numeric(triage[1,2:6] - triage[2,2:6])
+pt(sqrt(5) * mean(as.numeric(dif))/sd(as.numeric(dif)), 4)
+
+t.test(as.numeric(triage[2,2:6]), as.numeric(triage[1,2:6]), alternative = c("greater"), paired = TRUE)
+## p-value = 0.1405
+
+## Question 5:
+## Refer to the previous question. Give a 95% T confidence interval for the ratio of the waiting times (recall that the measurements were natural logged).
+new <- as.numeric(triage[1,2:6])
+old <- as.numeric(triage[2,2:6])
+exp(mean(new - old) + c(-1, 1) * qt(0.975, 4) * sd(new - old) / sqrt(5))
+## 0.09, 2.49
+
+## Question 6:
+## (X - Y) / (S_p * sqrt(1/n_x + 1/n_y)), S_p is common standard deviation
+Sp = sqrt((8*1.5^2 + 8*1.8^2)/16)
+pt(-4/(Sp * sqrt(1/9 + 1/9)), 16) * 2 ## 0.0001
+
+## Question 7:
+## [x + Z_(1-a) * s/sqrt(n), inf], WRONG
+## [x - Z_(1-a) * s/sqrt(n), inf]
+
+## Question 8:
+## Researchers would like to conduct a study of n healthy adults to detect a four year mean brain volume loss of .01 mm3. Assume that the standard deviation of four year volume loss in this population is .04 mm3. What would be the value of n needded for 90% power of type one error rate of 5% one sided test versus a null hypothesis of no volume loss?
+(-0.04 * (qnorm(.1) - qnorm(0.95))/0.01)^2 ## ~ 140 sample size
+qnorm(.05)
+((qnorm(0.05) * 0.04 )/ .01)^2
+## around 50, wrong
+
+## Question 9:
+pnorm(qnorm(.975) - (2/12)*sqrt(288))/2 ### 10% power
+pnorm(qnorm(.975) - (2/12)*sqrt(288)) ### 20% power, WRONG
+## around 40 %, wRONG
+pnorm(qnorm(.975)-(2 * sqrt(287)/12)) * 2
+
+## Question 10:
+## Power goees up
+## Power goes down, WRONG
+
+## Question 11:
+## Limits to 5%
+qnorm(0.95)
+
+## Question 12:
+## t distribution
+
+## Question 13:
+## X large, u0 small, sigma is small
